b9e11ef23abce34e41c8b2a099c6355bf3ab2a70
Christopher Eyre <chriseyre2000@gmail.com>
2020-01-26 18:30:31 +0000

    Create file

---
+++ count-codurance.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Count Codurance Staff
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       @chriseyre2000
+// @match        https://codurance.com/about-us/our-people/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('div.u-heading-v2-3--bottom h2').text( 'The Team (' + $('div.g-max-width-800').length + ')'  );
+})();
