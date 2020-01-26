818c32a5b1f97b3d5fc167ddf8ba11879c5a6731
Christopher Eyre <chriseyre2000@gmail.com>
2020-01-26 18:31:16 +0000

    Update count-codurance.js
    Now v2

--- count-codurance.js
+++ count-codurance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Count Codurance Staff
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       @chriseyre2000
 // @match        https://codurance.com/about-us/our-people/
